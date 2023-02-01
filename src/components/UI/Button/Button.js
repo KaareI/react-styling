4798629351b34cf014ad9863d602cc7ef3d7b27e
Kaarel <karlkaareltaniel.seppel@gmail.com>
2023-02-01 12:17:05 +0200

    “Use CSS modules for Button”

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,6 @@
-import styled from "styled-components";
-
-import './Button.css';
+import React from "react";
+import styles from './Button.module.css'
+/*import styled from "styled-components";
 
 const Button = styled.button`
   width: 100%;
@@ -26,14 +26,14 @@ const Button = styled.button`
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
-`
+`*/
 
-/*const Button = props => {
+const Button = props => {
     return (
-        <button type={props.type} className="button" onClick={props.onClick}>
+        <button type={props.type} className={styles.button} onClick={props.onClick}>
             {props.children}
         </button>
     );
-};*/
+};
 
 export default Button;
